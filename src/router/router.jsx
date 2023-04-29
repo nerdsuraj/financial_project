240ddeb1179889a159925beef9c8d9738fe1868c
NikhilM09 <mahadiknikhil28@gmail.com>
2023-04-29 22:09:31 +0530

    homepage initial laypout

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import Signup from "../pages/signup/signup";
 import Login from "../pages/signin/signin";
 import Navbar from "../pages/Navbar/navbar";
+import Homepage from "../pages/homepage/Homepage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function reactRouter() {
@@ -8,6 +9,7 @@ function reactRouter() {
         <div>
             <Router>
                 <Routes>
+                    <Route path="/" element={<Homepage/>} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/Login" element={<Login />} />
                     <Route path="/Dashboard" element={<Navbar />} />
