53512937ab55074e6c01959db6a6f96a315c20e3
NikhilM09 <mahadiknikhil28@gmail.com>
2023-07-17 00:50:28 +0530

    product page with categorywise data

--- src/pages/Productcard.js
+++ src/pages/Productcard.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Productcard = ({ image_link, title, price, affiliate_link,rating }) => {
     return (
         <Link to={affiliate_link} target="_blank" rel="noopener noreferrer" className="newscard text-decoration-none">
-            <div className="img-wrapper">
+            <div className="img-wrapper mb-1">
             {image_link!=="" ? (<img src={image_link} width="100%" height="100%" alt={title} />)
             : (<img src={dummy_img} width="100%" height="100%" alt="dummyimage" />)}
             </div>
-            <div className="text-white title_1">{title}</div>
-            <div className="text-white">₹ {price}</div>
-            <div className="text-white">Rating : {rating}/5</div>      
+            <div className="text-white px-2 title_1 mb-1">{title}</div>
+            <div className="text-white px-2 mb-1">₹ {price}</div>
+            <div className="text-white mx-2 mb-1 star_rating bg-success">⭐{rating}</div>      
         </Link>
     )
 }
--- src/pages/productPage/product.jsx
+++ src/pages/productPage/product.jsx
@@ -13,10 +13,12 @@ const Productpage = () => {
     const [selectedCategory, setSelectedCategory] = useState("");
     const [categorylist, setCategorylist] = useState([]);
     const [cardList, setCardList] = useState([]);
+    const [mastercardList, setMasterCardList] = useState([]);
 
 
     useEffect(() => {
         get_product_data();
+        console.log(selectedCategory, "selectedCategory");
     }, []);
 
 
@@ -30,10 +32,11 @@ const Productpage = () => {
             const response = await BusinessService().get_amazon_products(postData);
             console.log('response from the product list', response);
             if (response.status === 200) {
-                console.log('response.data', response.data);
+                console.log('response.data', response?.data);
                 //filter out the category from the response.data.category and store in the variable
-                setCategorylist(response.data.category);
-                setCardList(response.data.results);
+                setCategorylist(response?.data?.category);
+                setCardList(response?.data?.results);
+                setMasterCardList(response?.data?.results);
             }
         } catch (error) {
             console.error('Error fetching news:', error);
@@ -44,29 +47,59 @@ const Productpage = () => {
 
 
     const handleCategoryChange = (event) => {
+        // console.log("selected category", selectedCategory);
         setSelectedCategory(event.target.value);
+        const filteredProducts = mastercardList.filter((item) =>
+            item?.category === event.target.value
+        )
+        setCardList(filteredProducts);
     };
 
 
     return (
         <>
             <Navbar />
-                <div className="m-2">
-                    {/* <label htmlFor="category">Select Category:</label> */}
-                    <select id="category" value={selectedCategory} onChange={handleCategoryChange}  className="p-1">
-                        <option value=""> Select Category </option>
-                        {categorylist.map((category, index) => (
-                            <option key={index} value={category}>
-                                {category}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className="container-lg card-container mb-5">
-                {cardList.map((card,index)=>(
+            <div className="m-2 text-end">
+                {/* <label htmlFor="category">Select Category:</label> */}
+                <select id="category" value={selectedCategory} onChange={handleCategoryChange} className="p-1">
+                    <option value="">All</option>
+                    {categorylist.map((category, index) => (
+                        <option key={index} value={category} >
+                            {category}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="product_wrapper">
+                {
+                    categorylist.map((category, index) =>
+                        // console.log(cardList.filter((card)=>card.category===category))
+                        <>
+                            {(selectedCategory === category || selectedCategory === "") &&
+                                <>
+                                    <h3 className="mb-0" style={{ paddingLeft: "12.5px" }} key={index}>{category}</h3>
+                                    <hr></hr>
+                                </>}
+
+                            <div className="container-fluid product-container mb-5">
+                                {
+                                    
+                                        cardList.filter((card) => card.category === category).map((eachCard, index) => {
+                                            return (
+                                            <Productcard {...eachCard} key={index} />)
+                                        })
+                                    
+                                }
+                            </div>
+                        </>
+                    )
+                }
+            </div>
+            <div className="container-lg card-container mb-5">
+                {/* {cardList.map((card,index)=>(
                     <Productcard {...card} key={index}/>
-                ))}    
-                </div>
+                ))}     */}
+            </div>
             <Footer />
         </>
     );
